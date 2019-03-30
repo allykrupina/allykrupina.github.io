bde21ef8f2ae3d2046cc615e0da1863a186c68d6
allykrupina <36137765+allykrupina@users.noreply.github.com>
2019-03-30 22:08:16 +0200

    update js file
    update js file

--- saletest/app/assets/js/common.js
+++ saletest/app/assets/js/common.js
@@ -1,47 +1,53 @@
-const numbers = document.querySelectorAll('.btn-dark, .btn-orange:not(.js-equally)'),
-      operations = document.querySelectorAll('.btn-grey, .js-equally'),
+function Calculator() {
+
+  let result = document.querySelectorAll('.calculator-result p')[0],
+      expression = number = '',
+      legend = true,
       symb = ['+', '-', '.', '*', '/'];
 
-let result = document.querySelectorAll('.calculator-result p')[0],
-    expression = '',
-    number = '';
-
-Array.from(numbers).forEach((item, index) => item.addEventListener("click", () => changeNumber(item)));
-Array.from(operations).forEach((item, index) => item.addEventListener("click", () => changeResult(item)));
-
-const checkClass = (item, element) => {
-  return item.classList.contains(element);
-}
-
-const changeNumber = (item) => {
-  let symbol = item.innerHTML,
-      lastSymbol = expression.slice(-1);
-  if(checkClass(item, 'btn-dark')){
-    number += symbol;
-    result.innerHTML = number = number.replace( /^([^\.]*\.)|\./g, '$1' );
-  } else {
-    number = '';
-    result.innerHTML = symbol;
-    checkClass(item, 'js-division') ? symbol = "/" : checkClass(item, 'js-multiplication') ? symbol = "*" : null;
+  this.checkClass = (item, element) => {
+    return item.classList.contains(element);
   }
-  lastSymbol = checkClass(item, 'btn-orange') && !+lastSymbol && lastSymbol != 0 && lastSymbol != '.';
-  lastSymbol ? expression = expression.slice(0, -1) + symbol : expression += symbol;
-}
 
-const changeResult = (item) => {
-  if(symb.indexOf(expression.slice(0, 1)) > 2 || symb.indexOf(expression.slice(-1)) > -1 || checkClass(item, 'js-delete')){
-    result.innerHTML = '0';
-    expression = number = '';
-  } else {
-    if(checkClass(item, 'js-reverse')){
-      result.innerHTML = expression = number = (expression * -1).toString();
-    } else if (checkClass(item, 'js-percent')){
-      result.innerHTML = expression = number = (expression / 100).toString();
+  this.changeNumber = (item) => {
+    let symbol = item.innerHTML,
+        lastSymbol = expression.slice(-1);
+    this.checkClass(item, 'btn-dark') && !legend ? number = expression = '' : null;
+    legend = true;
+    if(this.checkClass(item, 'btn-dark')){
+      result.innerHTML = number += symbol;
     } else {
-      if(expression !== ''){
-        result.innerHTML = +eval(expression).toFixed(2).toString();
-        expression = number = '';
+      number = '';
+      result.innerHTML = symbol;
+      this.checkClass(item, 'js-division') ? symbol = "/" : this.checkClass(item, 'js-multiplication') ? symbol = "*" : null;
+    }
+    lastSymbol = this.checkClass(item, 'btn-orange') && !+lastSymbol && lastSymbol != 0 && lastSymbol != '.';
+    lastSymbol ? expression = expression.slice(0, -1) + symbol : expression += symbol;
+  }
+
+  this.changeResult = (item) => {
+    if(symb.indexOf(expression.slice(0, 1)) > 2 || symb.indexOf(expression.slice(-1)) > -1 || this.checkClass(item, 'js-delete')){
+      result.innerHTML = '0';
+      expression = number = '';
+    } else {
+      if(this.checkClass(item, 'js-reverse')){
+        result.innerHTML = expression = number = (expression * -1).toString();
+      } else if (this.checkClass(item, 'js-percent')){
+        result.innerHTML = expression = number = (expression / 100).toString();
+      } else {
+        if(expression !== ''){
+          result.innerHTML = +eval(expression).toFixed(2).toString();
+          expression = result.innerHTML;
+          legend = false;
+        }
       }
     }
   }
 }
+
+const calculator = new Calculator,
+      operations = document.querySelectorAll('.btn-grey, .js-equally'),
+      numbers = document.querySelectorAll('.btn-dark, .btn-orange:not(.js-equally)');
+
+Array.from(numbers).forEach((item, index) => item.addEventListener("click", () => calculator.changeNumber(item)));
+Array.from(operations).forEach((item, index) => item.addEventListener("click", () => calculator.changeResult(item)));
